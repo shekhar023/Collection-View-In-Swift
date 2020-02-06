696580cb2a2b8963046d6950279fc1fb17e56bb4
Shekhar <shekhar.0723@gmail.com>
2020-02-06 11:41:33 +0530

    Initial Commit

---
+++ AnitnoLabs Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnitnoLabs Assignment
+//
+//  Created by Shekhar Chaudhary on 06/02/20.
+//  Copyright © 2020 Shekhar Chaudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
